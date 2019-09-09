4f3b3c82c37edce20a91352ea4a566f55c3966cc
shumaikunkun <s1711520@gmail.com>
2019-09-09 18:50:35 +0900

    playerが左右ボタンで円周上を回る

--- ShotZumou/Assets/Scripts/Game/ForShumaPlayer.cs
+++ ShotZumou/Assets/Scripts/Game/ForShumaPlayer.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class ForShumaPlayer : MonoBehaviour
 {
-    public GameObject player;
-    //  動かしたいプレイヤー
-    bool right = false;
-    //  右ボタンを押しているかの真偽値
-    bool left = false;
-    //  左ボタンを押しているかの真偽値
+    public float speed=1.0f;
 
-
-    // Use this for initialization
     void Start()
     {
-		Debug.Log("start");
+
 	}
 
     // Update is called once per frame
@@ -23,78 +16,19 @@ public class ForShumaPlayer : MonoBehaviour
     {
 		//Debug.Log("update");
 
-		if (Input.GetKey(KeyCode.UpArrow))
+		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			// 処理
-            Debug.Log("aaa");
-			transform.position += new Vector3(5.0f * Time.deltaTime, 0, 0);
+			//Debug.Log("aaa");
+            transform.RotateAround(transform.position, Vector3.up, speed);
 		}
 
-		if (Input.GetKey(KeyCode.DownArrow))
+		if (Input.GetKey(KeyCode.RightArrow))
 		{
 			// 処理
-			Debug.Log("bbb");
-			transform.position += new Vector3(-5.0f * Time.deltaTime, 0, 0);
+			//Debug.Log("bbb");
+			transform.RotateAround(transform.position, Vector3.up, -speed);
 		}
-
-
-
-		if (right)
-        {
-            goright();
-            //          右に動かすためのメソッドを呼び出す
-        }
-        else if (left)
-        {
-            goleft();
-            //          左に動かすためのメソッドを呼び出す
-        }
-        else
-        {
-            //          ボタンを押していない時
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            //          プレイヤーを元の角度に戻す
-        }
-    }
-
-    public void rPushDown()
-    {
-        //      右ボタンを押している間
-        right = true;
-    }
-
-    public void rPushUp()
-    {
-        //      右ボタンを押すのをやめた時
-        right = false;
-    }
-
-    public void lPushDown()
-    {
-        //      左ボタンを押している間
-        left = true;
-    }
-
-    public void lPushUp()
-    {
-        //      左ボタンを押すのをやめた時
-        left = false;
-    }
-
-    public void goright()
-    {
-
-            transform.position += new Vector3(5.0f * Time.deltaTime, 0, 0);
-            //          プレイヤーをx軸方向に秒速5.0fで動かす
-
-    }
-
-    public void goleft()
-    {
-
-            transform.position += new Vector3(-5.0f * Time.deltaTime, 0, 0);
-            //          プレイヤーをx軸方向に秒速-5.0fで動かす
-
     }
     
 
