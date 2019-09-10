233a90e050712ad410d26b832375cea04493f23d
Okae Jin <mokafogeru@gmail.com>
2019-09-10 15:52:39 +0900

    update: 中間発表用

--- ShotZumou/Assets/Scripts/Game/Core/PlayerRotation.cs
+++ ShotZumou/Assets/Scripts/Game/Core/PlayerRotation.cs
@@ -14,7 +14,6 @@ public class PlayerRotation : MonoBehaviour
     float horizontalAngle = 0f; // 水平方向の回転量を保存
     float verticalAngle = 0f; // 垂直方向の回転量を保存​
 
-#if UNITY_EDITOR
     void Update()
     {
         //// 入力による回転量を取得
@@ -47,5 +46,4 @@ public class PlayerRotation : MonoBehaviour
         //}
 
     }
-#endif
 }
