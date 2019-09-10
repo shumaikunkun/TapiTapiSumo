08e093d5766934b5ff0ab6f45408a5a036a1c7d8
shumaikunkun <s1711520@gmail.com>
2019-09-10 22:51:41 +0900

    サドンデスタイムになったら、フィールド縮小とプレイヤーも中心に移動

---
+++ ShotZumou/Assets/Scripts/Game/Core/Stage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if ("サドンデスタイム")
+        //{
+            //Debug.Log(transform.localScale.x);
+            //Debug.Log(transform.lossyScale.x);
+            transform.localScale -= new Vector3(0.01f, 0, 0.01f);
+            //時間とともにフィールドが縮小
+
+            //Debug.Log(GameObject.FindWithTag("Player").transform.position.z);
+            GameObject.FindWithTag("Player1").transform.position += new Vector3(0, 0, 0.005f);
+            GameObject.FindWithTag("Player2").transform.position -= new Vector3(0, 0, 0.005f);
+
+
+        //}
+    }
+}
