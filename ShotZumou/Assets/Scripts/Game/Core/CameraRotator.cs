3d76ca7d7248b8dd865cae12b5f3325c9ca195de
わかちこ。 <wakachiko0623@gmail.com>
2019-09-10 10:53:28 +0900

    駒のプレファブ化

--- ShotZumou/Assets/Scripts/Game/Core/CameraRotator.cs
+++ ShotZumou/Assets/Scripts/Game/Core/CameraRotator.cs
@@ -9,7 +9,7 @@ public class CameraRotator : MonoBehaviour
     float horizontalAngle = 0f; // 水平方向の回転量を保存
     float verticalAngle = 0f;   // 垂直方向の回転量を保存
 
-#if UNITY_EDITOR
+
     void Update()
     {
         // 入力による回転量を取得
@@ -26,5 +26,4 @@ public class CameraRotator : MonoBehaviour
         // Transformコンポーネントに回転量を適用する
         transform.rotation = Quaternion.Euler(verticalAngle, horizontalAngle, 0f);
     }
-#endif
 }
