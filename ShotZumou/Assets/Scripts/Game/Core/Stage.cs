bf27b9c5e47fc3425d349b9edec01f94e226c382
Okae Jin <mokafogeru@gmail.com>
2019-09-11 11:13:36 +0900

    update: 全部をマージ

--- ShotZumou/Assets/Scripts/Game/Core/Stage.cs
+++ ShotZumou/Assets/Scripts/Game/Core/Stage.cs
@@ -7,7 +7,7 @@ public class Stage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,15 +16,15 @@ public class Stage : MonoBehaviour
 
         //if ("サドンデスタイム")
         //{
-            //Debug.Log(transform.localScale.x);
-            //Debug.Log(transform.lossyScale.x);
-            transform.localScale -= new Vector3(0.01f, 0, 0.01f);
-            //時間とともにフィールドが縮小
+        //Debug.Log(transform.localScale.x);
+        //Debug.Log(transform.lossyScale.x);
+        transform.localScale -= new Vector3(0.01f, 0, 0.01f);
+        //時間とともにフィールドが縮小
 
-            //Debug.Log(GameObject.FindWithTag("Player").transform.position.z);
-            GameObject.FindWithTag("Player1").transform.position += new Vector3(0, 0, 0.005f);
-            //GameObject.FindWithTag("Player2").transform.position -= new Vector3(0, 0, 0.005f);
-            //マルチプレイ時上のスクリプトをコメントアウト解除する
+        //Debug.Log(GameObject.FindWithTag("Player").transform.position.z);
+        GameObject.FindWithTag("Player1").transform.localPosition += new Vector3(0, 0, 0.005f);
+        GameObject.FindWithTag("Player2").transform.localPosition += new Vector3(0, 0, 0.005f);
+        //マルチプレイ時上のスクリプトをコメントアウト解除する
 
         //}
     }
