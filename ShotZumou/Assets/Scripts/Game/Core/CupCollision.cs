838097ccd3fcd1c6d3219cc462459dd8833fdfde
Okae Jin <mokafogeru@gmail.com>
2019-09-11 13:47:24 +0900

    update: Game中にタピボトルがぶつかったら音がなるように

---
+++ ShotZumou/Assets/Scripts/Game/Core/CupCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player1のやつだけにつける
+public class CupCollision : MonoBehaviour
+{
+    [SerializeField] AudioClip colSound;
+    AudioSource colAudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colAudioSource = gameObject.AddComponent<AudioSource>();
+        colAudioSource.clip = colSound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        string tag = other.gameObject.tag;
+        Debug.Log("dur");
+        if (tag == "Object2")
+        {
+            colAudioSource.volume = 0.5f;
+            colAudioSource.Play();
+            Debug.Log("kaki");
+        }
+    }
+}
